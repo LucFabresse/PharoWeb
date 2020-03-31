ac224c2b9cd76fe38939aa7e2cf9508c8afdf834
LucFabresse <luc.fabresse@gmail.com>
2020-03-31 11:51:56 +0200

    cleanImage.st after successful build

--- scripts/runSt.sh
+++ scripts/runSt.sh
@@ -15,12 +15,8 @@ readonly ANSI_BLUE="\033[34m"
 readonly ANSI_RESET="\033[0m"
 readonly ANSI_CLEAR="\033[0K"
 
-echo -e "${ANSI_BOLD}${ANSI_BLUE}[Starting] CleanImage.sh${ANSI_RESET}"
+echo -e "${ANSI_BOLD}${ANSI_GREEN}[Starting] $0 $@${ANSI_RESET}"
 
-script="
-Transcript crShow: (1+1) asString
-"
+"${SMALLTALK_CI_VM}" "${SMALLTALK_CI_IMAGE}" --no-default-preferences $@
 
-"${SMALLTALK_CI_VM}" "${SMALLTALK_CI_IMAGE}" --no-default-preferences eval "${script}"
-
-echo -e "${ANSI_BOLD}${ANSI_BLUE}[DONE] CleanImage.sh${ANSI_RESET}"
+echo -e "${ANSI_BOLD}${ANSI_GREEN}[DONE] $0 $@${ANSI_RESET}"
